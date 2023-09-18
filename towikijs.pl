f2ab7e7d0888b160a3af9ad4c34888a83f3bc885
brickhousewindow <brickhouse@eclipso.email>
2023-09-18 14:34:56 +0200

    rewrote script to use the external TreeWalk module.

--- towikijs.pl
+++ towikijs.pl
@@ -6,6 +6,7 @@ use Cwd;
 
 BEGIN { unshift @INC, '.'; }
 use MacroProcess;
+use TreeWalk;
 
 use YAML::XS;
 use File::Slurp;
@@ -52,60 +53,68 @@ sub debug;
 sub append_attachments;
 
 foreach my $id (keys %$spaces){
+    next if ( $id eq "users" );
     debug("processing space id $id.");
-    process_child( \%{ $spaces->{ $id } } );
+    process_space( \%{ $spaces->{ $id } } );
 }
 
-sub process_child {
-    my $subtree = shift @_;
-    debug("Processing page with title ".$subtree->{"title"}.".");
+sub process_space {
+    my $space = shift @_;
+    TreeWalk::walk( $space );
+    my @node_list = @TreeWalk::node_list;
+    my $signal = "UP";
 
-    if ( defined $subtree->{ "child" }
-	 and scalar keys %{ $subtree->{ "child" } } ){
-	my $dir_name = sanitize_name( $subtree->{ "title" } );
-	debug("creating and entering directory $dir_name.");
-	mkdir $dir_name
-	    || die "cannot create directory: $!";
-	chdir $dir_name
-	    || die "could not change into directory: $!";
-	debug(qq#current cwd is # . cwd() . qq#.#);
-    }
+    foreach my $node ( @node_list ){
 
+	if ( $node eq "UP" ) {
+	    $signal = $node;
+	    debug("Signal switched to $signal.");
+	}
+	elsif ( $node eq "DOWN" ) {
+	    $signal = $node;
+	    debug("Signal switched to $signal.");
+	    debug("going one directory up.");
+	    chdir ".." || die "could not change into directory: $!";
+	    debug("current cwd is " . cwd() . ".");
+	    
+	} 
+	elsif ( $node eq "CURRENT" ) {
+	    $signal = $node;
+	    debug("Signal switched to $signal.");
+	}
+	else {
+	
 =comment
 	
 creating a file to place the content inside.
-because a directory cannot have content the body is put
+because a directory cannot have a content the body is put
 into a file named after the directory.
 pages inside Confluence all have unique names
 inside a space therefor conflicts are not expected.
 
 =cut
-    if ( defined $subtree->{ "body" }
-	 and length $subtree->{ "body" } ){
+	    if ( $signal eq "UP" ) {
+		my $dir_name = sanitize_name( $node->{ "title" } );
+		debug("creating and entering directory $dir_name.");
+		mkdir $dir_name
+		    || die "cannot create directory: $!";
+		chdir $dir_name
+		    || die "could not change into directory: $!";
+		debug("current cwd is " . cwd() . ".");
+	    }
 
-        MacroProcess::macros_convert( \%{$subtree}, \%{ $spaces->{"users"} } );
-
-	append_attachments( \%{$subtree} );
-	
-	my $filename = sanitize_name( $subtree->{ "title" } ) . ".html";
-	debug("creating file with filename $filename.");
-	write_file( $filename,
-		    {binmode => ':utf8'},
-		    $subtree->{ "body" }
-	    ) || die "cannot write to file $filename: $!";
-    }
-
-    foreach my $child_id ( keys %{ $subtree->{ "child" } } ){
-	debug("processing child $child_id.");
-	process_child( \%{ $subtree->{ "child" }->{ $child_id } } );
-    }
-    debug("all children were processed.");
-
-    if ( defined $subtree->{ "child" }
-	 and scalar keys %{ $subtree->{ "child" } } ){
-	debug("going one directory up.");
-	chdir "..";
-	debug(qq#current cwd is # . cwd() . qq#.#);
+	    debug("Processing page with title ".$node->{"title"}.".");
+	    MacroProcess::macros_convert( \%{$node}, \%{ $spaces->{"users"} } );
+	    append_attachments( \%{$node} );
+	    
+	    my $filename = sanitize_name( $node->{ "title" } ) . ".html";
+	    debug("creating file with filename $filename.");
+	    write_file( $filename,
+			{binmode => ':utf8'},
+			$node->{ "body" }
+		) || die "cannot write to file $filename: $!";
+	    $signal = "";
+	}
     }
 }
 
